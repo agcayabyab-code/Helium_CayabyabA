006f749b2816402e2bdfea6264d55eabf31fb8ea
Andy <agcayabyab@carc.pshs.edu.ph>
2026-08-24 15:10:30 +0800

    Added more error handling

--- zodiacHeliumCayabyab.py
+++ zodiacHeliumCayabyab.py
@@ -1,5 +1,9 @@
 #birthyear input and initialization of zodiac list
-birthyear = int(input("Enter your birth year: "))
+try:#error handling for non-integer inputs
+    birthyear = int(input("Enter your birth year: "))
+except ValueError:
+    print("Invalid input. Please enter an integer value for the birth year.")
+    exit()
 zodiacs = ["Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)",
            "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", 
            "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]

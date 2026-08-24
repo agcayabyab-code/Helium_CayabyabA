ddb7a42a2201c18e706198abf7b71a65852d7626
Andy <agcayabyab@carc.pshs.edu.ph>
2026-08-24 15:11:53 +0800

    Comments

--- zodiacHeliumCayabyab.py
+++ zodiacHeliumCayabyab.py
@@ -4,6 +4,7 @@ try:#error handling for non-integer inputs
 except ValueError:
     print("Invalid input. Please enter an integer value for the birth year.")
     exit()
+#Zodiac list containing the 12 Chinese zodiac signs with their chinese characters.
 zodiacs = ["Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)",
            "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", 
            "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
